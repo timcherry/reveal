675a90e9deed60238ba76506941fcf38a43ecfd5
Tim Cherry <timcherry21@gmail.com>
2014-05-27 22:37:47 -0700

    semi working api

---
+++ reveal/revealAPIModel.h
@@ -0,0 +1,16 @@
+//
+//  revealAPIModel.h
+//  reveal
+//
+//  Created by Tim Cherry on 5/7/14.
+//  Copyright (c) 2014 Tim Cherry. All rights reserved.
+//
+
+
+
+#import <Foundation/Foundation.h>
+
+@interface revealAPIModel : NSObject
+
+
+@end
---
+++ reveal/revealAPIModel.m
@@ -0,0 +1,13 @@
+//
+//  revealAPIModel.m
+//  reveal
+//
+//  Created by Tim Cherry on 5/7/14.
+//  Copyright (c) 2014 Tim Cherry. All rights reserved.
+//
+
+#import "revealAPIModel.h"
+
+@implementation revealAPIModel
+
+@end
